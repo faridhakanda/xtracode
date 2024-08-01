aaa79f258a0a748d8c8c6f5c2ae961e9db423b58
faridhakanda <faridakanda1729@gmail.com>
2024-08-01 23:57:55 +0600

    initial commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,11 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <Header />
+      <h1>Xtra Code!</h1>
+      <p>Xtra code is my dream project for my startup business!</p>
+      <h1>Make a startup company with internet</h1>
+      <p>I love to make something new and also love build something!</p>
+      <p>Building and Fixing is make a men stronger.</p>
     </main>
   );
 }
